ca3be055b541e256b8923f28b799a22f37938b30
Goldy Lokhande <goldylokhande12@gmail.com>
2026-07-15 12:59:28 +0530

    .

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -31,7 +31,7 @@ export default function Resume({ resumeUrl, name }) {
             </div>
 
             {resumeUrl ? (
-              <a href={resumeUrl} download target="_blank" rel="noreferrer" className="btn btn-primary">
+              <a href={resumeUrl} Goldy-Lokhande target="_blank" rel="noreferrer" className="btn btn-primary">
                 <FileDown size={16} /> Download CV
               </a>
             ) : (
